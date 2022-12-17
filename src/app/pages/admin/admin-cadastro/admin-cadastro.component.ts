039c61543292cd80cac7ec1cf211d8acadabc68f
Pedro Henrique Maia <phmaia2018@gmail.com>
2022-12-17 20:57:27 -0300

    fix: Ajusta mascara no input de cpf do admin

--- src/app/pages/admin/admin-cadastro/admin-cadastro.component.ts
+++ src/app/pages/admin/admin-cadastro/admin-cadastro.component.ts
@@ -30,4 +30,13 @@ export class AdminCadastroComponent implements OnInit {
       (err) => this.mensagemErro = true 
       )
   }
+  
+  cpfMask(event: any) {
+    this.cpf = event.target.value
+      .replace(/\D/g, '') // substitui qualquer caracter que nao seja numero por nada
+      .replace(/(\d{3})(\d)/, '$1.$2') // captura 2 grupos de numero o primeiro de 3 e o segundo de 1, apos capturar o primeiro grupo ele adiciona um ponto antes do segundo grupo de numero
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+      .replace(/(-\d{2})\d+?$/, '$1') // captura 2 numeros seguidos de um traço e não deixa ser digitado ma
+  }
 }
